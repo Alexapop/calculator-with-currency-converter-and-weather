729e3cba6ad4eb9381181954d09e2f08578dcbe6
Ioana <ioana3popescu@gmail.com>
2026-07-18 19:51:23 +0200

    feat: add static calculator interface

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,23 @@
 <script setup>
 import AppHeader from "@/components/header/AppHeader.vue";
 import AppFooter from "./components/footer/AppFooter.vue";
+import AppCalculator from "./components/calculator/AppCalculator.vue";
 </script>
 
 <template>
-  <AppHeader />
-  
+  <div class="container py-4 py-md-5">
+    <AppHeader />
 
-  <main>
-   
-  </main>
-  <AppFooter/>
+    <main>
+      <div class="row g-4">
+        <div class="col-12 col-md-6 col-xl-4">
+          <AppCalculator />
+        </div>
+      </div>
+    </main>
+
+    <AppFooter />
+  </div>
 </template>
 
 

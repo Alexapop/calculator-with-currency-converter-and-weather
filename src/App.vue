1d141ad8fe390e57d389187db975a2b723a85f44
Ioana <ioana3popescu@gmail.com>
2026-07-18 20:34:05 +0200

    fix: adjust layout for calculator, currency convertor and header

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,16 @@ import CurrencyConvertor from "./components/currency/CurrencyConvertor.vue";
 
 <template>
   <div class="container py-4 py-md-5">
-    <AppHeader />
+    <div class="page-header">
+      <AppHeader />
+    </div>
 
     <main>
-      <div class="row g-4">
-        <div class="col-12 col-md-6 col-xl-4">
+      <div class="widgets-row row justify-content-center g-4">
+        <div class="col-12 widget-column">
           <AppCalculator />
         </div>
-        <div class="col-12 col-md-6 col-xl-4">
+        <div class="col-12 widget-column">
           <CurrencyConvertor />
         </div>
       </div>
@@ -23,5 +25,22 @@ import CurrencyConvertor from "./components/currency/CurrencyConvertor.vue";
     <AppFooter />
   </div>
 </template>
+
+<style lang="scss" scoped>
+@media (min-width: 992px) {
+  .page-header {
+    margin-inline: 5%;
+  }
+
+  .widgets-row {
+    --bs-gutter-x: 2rem;
+  }
+
+  .widget-column {
+    flex: 0 0 auto;
+    width: 45%;
+  }
+}
+</style>
 
 
